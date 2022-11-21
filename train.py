a0649906b9eb1f99e25e4ce2dd9ae7787e8687db
RushivArora <rushivarora@gmail.com>
2022-11-21 15:59:38 -0500

    Train file for inits

---
+++ train.py
@@ -0,0 +1,12 @@
+import argparse
+import pandas as pd
+import numpy as np
+
+class Train_Scratch_Model():
+    def __init__(self):
+
+
+    def train_shadow_model(self):
+
+    def train_target_model(self):
+        
